e55add5c9afba502ef838f41b89028bc6cb9c57b
Dan Shreim <dan@snapjay.com>
2013-08-29 11:30:05 -0400

    Using Angulat 1.2.0rc1

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,8 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+angular.module('myApp', ['ngRoute', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/grid', {templateUrl: 'partials/grid.html', controller: 'MyCtrl1'});
     $routeProvider.when('/buttons', {templateUrl: 'partials/buttons.html', controller: 'MyCtrl2'});
